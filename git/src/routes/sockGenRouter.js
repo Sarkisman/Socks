7ab22c9f8cf470315cabc92f4ac4847c249ed288
Sarkis <sarkis_strelok@mail.ru>
2022-12-02 12:11:38 +0300

    clining

--- git/src/routes/sockGenRouter.js
+++ git/src/routes/sockGenRouter.js
@@ -14,12 +14,11 @@ sockgen.get('/', async (req, res) => {
 });
 
 sockgen.post('/postsock', async (req, res) => {
-  req.body.userId = req.session?.user?.id; // записываем из запроса req
-  req.body.bascetSt = true; // записываем из запроса req
+  req.body.userId = req.session?.user?.id;
+  req.body.bascetSt = true;
   const {
     color, pattern, img, userId, favorSt, bascetSt,
   } = req.body;
-  // await Sock.create(req.body); // записываем из запроса req
   await Sock.findOrCreate({
     where: {
       color, pattern, img, userId,
@@ -27,24 +26,25 @@ sockgen.post('/postsock', async (req, res) => {
     defaults: {
       color, pattern, img, userId, favorSt, bascetSt,
     },
-  }); // записываем из запроса req
-  // const currMemeWithUsername = await Sock.findOne({ where: { id: currMeme.id }, include: User });
-  // req.body.username = req.session?.user?.username;
-  // console.log(req.body, 'то что пришло в req.body');
+  });
   res.sendStatus(200);
-//   res.json(currMemeWithUsername); // отправляем только что созданный мем обратно на фронт
 });
 
 sockgen.post('/likesock', async (req, res) => {
-  req.body.userId = req.session?.user?.id; // записываем из запроса req
-  req.body.favorSt = true; // записываем из запроса req
-
-  await Sock.create(req.body); // записываем из запроса req
-  // const currMemeWithUsername = await Sock.findOne({ where: { id: currMeme.id }, include: User });
-  // req.body.username = req.session?.user?.username;
-  // console.log(req.body, 'то что пришло в req.body');
+  req.body.userId = req.session?.user?.id;
+  req.body.favorSt = true;
+  const {
+    color, pattern, img, userId, favorSt, bascetSt,
+  } = req.body;
+  await Sock.findOrCreate({
+    where: {
+      color, pattern, img, userId,
+    },
+    defaults: {
+      color, pattern, img, userId, favorSt, bascetSt,
+    },
+  });
   res.sendStatus(200);
-//   res.json(currMemeWithUsername); // отправляем только что созданный мем обратно на фронт
 });
 
 export default sockgen;
